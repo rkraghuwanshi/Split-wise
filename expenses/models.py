2e38399b14b46f086dd78cb9655733939788a1e1
rkraghuwanshi <rkraghuwanshi28@gmail.com>
2023-10-07 23:49:14 +0530

    add user model

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,58 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
+
+
+class UserProfileManager(BaseUserManager):
+    """ Manager for user profiles """
+
+    def create_user(self, email, name, password=None,mobile=None) -> "UserProfile":
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('Invalid Email')
+        # normalize email, convert second half to lowercase
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class UserProfile(AbstractBaseUser):
+    """ Database model for users in system """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    mobile = models.CharField(max_length=13, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self) -> str:
+        """
+        Retrieve full name of user
+        :return: str
+        """
+        return self.name
+
+    def get_short_name(self) -> str:
+        """
+        Retrieve full name of user
+        :return: str
+        """
+        return self.name
+
+    def __str__(self) -> str:
+        """
+        Return String representation of User
+        :return: str
+        """
+        return f"Email: {self.email}, Name:{self.name}"
