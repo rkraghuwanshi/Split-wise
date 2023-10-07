1e7f23411b4161ac7266bba5c709c66c0fa26688
rkraghuwanshi <rkraghuwanshi28@gmail.com>
2023-10-07 23:53:32 +0530

    added required models

--- expenses/models.py
+++ expenses/models.py
@@ -56,3 +56,46 @@ class UserProfile(AbstractBaseUser):
         :return: str
         """
         return f"Email: {self.email}, Name:{self.name}"
+
+class Debt(models.Model):
+    from_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='from_user')
+    to_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='to_user')
+    amount = models.DecimalField(default=0,max_digits=9,decimal_places=2)
+
+    def __str__(self):
+        return f'{self.to_user.name} owes {self.amount} to {self.from_user.name}'
+
+
+class Group(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    group_name = models.CharField(max_length=255, unique=True)
+    debts = models.ManyToManyField(Debt, null=True)
+    members = models.ManyToManyField(UserProfile)
+
+    def __str__(self):
+        return f'{self.group_name}'
+
+
+class ExpenseUser(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    paid_share = models.DecimalField(default=0,max_digits=9,decimal_places=2)
+    owed_share = models.DecimalField(default=0,max_digits=9,decimal_places=2)
+    net_balance = models.DecimalField(default=0,max_digits=9,decimal_places=2)
+    def __str__(self):
+        return f'{self.user.name} lent {self.owed_share} net_balance {self.net_balance}'
+
+
+class Expense(models.Model):
+    name = models.CharField(max_length=255, unique=False)
+    expense_group = models.ForeignKey(Group, on_delete=models.DO_NOTHING, null=True, db_constraint=False)
+    description = models.CharField(max_length=255)
+    payment = models.BooleanField(default=False)
+    amount = models.DecimalField(default=0,max_digits=9,decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+    repayments = models.ManyToManyField(Debt)
+    users = models.ManyToManyField(ExpenseUser)
+    transaction_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    def __str__(self):
+        return f'{self.name}'
